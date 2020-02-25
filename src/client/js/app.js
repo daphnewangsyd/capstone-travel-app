5e1eebf77ff8595b88d9a2cfcc57e3203085e630
Alex Lei <luckyhokey@gmail.com>
2020-02-25 20:47:58 +1100

    Finished: - Add Geonames API

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -1,8 +1,8 @@
 /* Global Variables */
 
-const BASE_URL_WEATHER='http://api.openweathermap.org/data/2.5/weather?';
-// Personal API Key for OpenWeatherMap API
-const API_KEY='500350f8669fa07140fa893d35b4a064';
+const GEONAMES_USERNAME='miuwusoftpaw';
+// Personal API Key for Dark Sky API
+const DARK_SKY_API_KEY='aabde7e995d82645e83cc7744086b6cd';
 
 /* Main Functions */
 /* Load all trips */
@@ -43,7 +43,7 @@ async function renderTripList(data) {
             '                </div>\n' +
             '                <div class="row">\n' +
             '                    <div class="column">\n' +
-            '                        <img src="https://media.cntraveler.com/photos/5d8cf7d5db6acf000833e6cc/4:3/w_420,c_limit/Eiffel-Tower_GettyImages-1060266626.jpg" alt="image" data-id="'+line.id+'">\n' +
+            '                        <img class="photo" src="https://media.cntraveler.com/photos/5d8cf7d5db6acf000833e6cc/4:3/w_420,c_limit/Eiffel-Tower_GettyImages-1060266626.jpg" alt="image" data-id="'+line.id+'">\n' +
             '                    </div>\n' +
             '                    <div class="column">\n' +
             '                        <p>The trip is '+daysTo+' days away.</p>\n' +
@@ -59,7 +59,28 @@ async function renderTripList(data) {
     });
     document.getElementById("trips-list").innerHTML = html;
     console.log("Loading trip data...Done");
-    return "OK";
+    return await renderWeather(data);
+}
+
+/* Function to fetch weather information and render the list with the results */
+async function renderWeather(data){
+    for (const line of data) {
+        console.log("Processing: "+line.location);
+        try {
+            const response = await fetch('http://api.geonames.org/searchJSON?q='+line.location+'&username='+GEONAMES_USERNAME);
+            const data = response.json();
+            if (data.totalResultsCount === 0){
+                continue;
+            }
+            await data.then(value => console.log(value.geonames[0]));
+        } catch (e) {
+            console.log('error', e);
+        }
+    }
+}
+
+async function fetchWeather(parsedLocation){
+
 }
 
 /* Functions called by event listener */
@@ -151,55 +172,5 @@ function getDateDifference(dateFrom, dateTo) {
     // Convert back to days and return
     return Math.round(differenceMs / ONE_DAY);
 }
-//
-// /* Function to GET Web API Data*/
-// const getWeather = async (baseUrl = '', zip = '', key = '')=>{
-//     const response = await fetch(baseUrl+'zip='+zip+',us&APPID='+key, {
-//         method: 'GET'
-//     });
-//     try {
-//         const weatherData = await response.json();
-//         console.log(weatherData);
-//         return weatherData;
-//     }catch(error) {
-//         console.log("error", error);
-//     }
-// };
-//
-// /* Function to POST data */
-// const postData = async (data) => {
-//     const transformedData = {
-//         temperature: data.main.temp,
-//         date: newDate,
-//         userResponse: document.getElementById('feelings').value
-//     };
-//     console.log(transformedData);
-//     const response = await fetch('/add', {
-//         method: 'POST',
-//         credentials: "same-origin",
-//         headers: {
-//             'Content-Type': 'application/json'
-//         },
-//         body: JSON.stringify(transformedData)
-//     });
-//     try {
-//         return await response.json();
-//     } catch (e) {
-//         console.log("error", e);
-//     }
-// };
-//
-// /* Function to GET Project Data */
-// const updateData = async () => {
-//     const response = await fetch('/all');
-//     try {
-//         const data = await response.json();
-//         document.getElementById('date').innerHTML = data.date;
-//         document.getElementById('temp').innerHTML = data.temperature;
-//         document.getElementById('content').innerHTML = data.userResponse;
-//     } catch (e) {
-//         console.log('error', e);
-//     }
-// };
 
 export {onSaveTrip, onDeleteTrip, onAddTrip, onCancelTrip, loadTrips, onNotesChanged}
